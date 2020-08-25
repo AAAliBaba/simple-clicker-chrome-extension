25ebfca9a13c84ac2b93ca652c9d1bceabd6ad4c
Arafat Arman <arafatarman616@gmail.com>
2020-08-25 17:04:31 -0400

    added simple styling

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var count = 0;
 
-document.getElementById("btn_add").addEventListener('click', onAddClick, false);
-document.getElementById("btn_sub").addEventListener('click', onSubClick, false);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("btn_add").addEventListener('click', onAddClick, false);
+    document.getElementById("btn_sub").addEventListener('click', onSubClick, false);
+}, false);
 
 function onAddClick () {
     count++;
