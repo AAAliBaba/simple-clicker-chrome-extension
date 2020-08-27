ccac54f7d2af4b415c765267b5ffd9208bc30082
Arafat Arman <arafatarman616@gmail.com>
2020-08-27 15:37:41 -0400

    added persistent storage

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //adding event listeners to the buttons
     document.getElementById("btn_add").addEventListener('click', onAddClick, false);
     document.getElementById("btn_sub").addEventListener('click', onSubClick, false);
-    chrome.storage.sync.set({'count': 0});
+
+    //adding storage listener
     chrome.storage.onChanged.addListener(setCount);
+
+    //initializing "count" in storage if it is not already
+    chrome.storage.sync.get('count', function(data) {
+        data == undefined ? chrome.storage.sync.set({'count': 0}) : chrome.storage.sync.set({'count': data['count']});
+        setCount();
+    })
 }, false);
 
 function onAddClick () {
+    //gets current count and increments by one
     chrome.storage.sync.get('count', function(data) {
-        let c = data['count'] + 1;
-        console.log(c);
-        chrome.storage.sync.set({'count': c});
+        chrome.storage.sync.set({'count': data['count'] + 1});
     });
 }
 
 function onSubClick () {
+    //gets current count and decrements by one
     chrome.storage.sync.get('count', function(data) {
-        let c = data['count'] - 1;
-        chrome.storage.sync.set({'count': c});
-        console.log(c);
+        chrome.storage.sync.set({'count': data['count'] - 1});
     });
 }
 
 function setCount () {
+    //updates the counter
     chrome.storage.sync.get('count', function(data) {
         document.getElementById("cnt").innerText = `${data['count']}`;
-        console.log(data['count']);
     })
 }
